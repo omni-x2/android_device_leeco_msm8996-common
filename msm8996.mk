92ff3add4537b86668729fffd54bcfb2ce845a27
Home <mouyase@qq.com>
2018-07-01 19:43:49 -0600

    msm8996: Update thermal-engine-8996.conf
    Change-Id: I8c977f4311382e836add2c70a9456649fe5d825f

--- msm8996.mk
+++ msm8996.mk
@@ -369,7 +369,7 @@ PRODUCT_PACKAGES += \
     thermal.msm8996
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+    $(LOCAL_PATH)/configs/thermal-engine-8996.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996.conf
 
 # USB
 PRODUCT_PACKAGES += \
