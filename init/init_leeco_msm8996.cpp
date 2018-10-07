c9134d8fe3d21317df80fcc9aab9c9e0e8325d04
andr68rus <ashtmb@gmail.com>
2018-10-07 22:06:37 +0300

    msm8996-common: init: fix compilation on omni

--- init/init_leeco_msm8996.cpp
+++ init/init_leeco_msm8996.cpp
@@ -44,10 +44,11 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
-#include "vendor_init.h"
 #include "property_service.h"
 
 #define DEVINFO_FILE "/dev/block/sde21"
+
+#define TARGET_INIT_VENDOR_LIB
 
 using android::base::Trim;
 using android::base::GetProperty;
