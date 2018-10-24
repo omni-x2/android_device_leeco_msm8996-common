2cb6668d60291882ddafdb5b0b867e9c0324367f
andr68rus <ashtmb@gmail.com>
2018-10-24 08:04:24 +0300

    Disable PRODUCT_BOOT_JARS += telephony-ext because it added to NOS vendor: https://github.com/nitrogen-project/android_vendor_nitrogen/commit/697a97a898df33c538eec5c0bfc5dfd4426455b0

--- msm8996.mk
+++ msm8996.mk
@@ -350,7 +350,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
+#PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # TextClassifier smart selection model files
