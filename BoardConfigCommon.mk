7206d178e731ccf19bb6f708614419e1a6b9cf8e
andr68rus <ashtmb@gmail.com>
2018-11-01 11:11:22 +0300

    msm8996-common: init: update init, add back spectrum

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -213,7 +213,7 @@ TARGET_USES_MKE2FS := true
 
 # Power
 TARGET_HAS_NO_WIFI_STATS := true
-TARGET_USES_INTERACTION_BOOST := true
+TARGET_USES_AOSP := true
 
 # Recovery
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_leeco
