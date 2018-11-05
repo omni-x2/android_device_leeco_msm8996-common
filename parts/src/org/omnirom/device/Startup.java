82b49526b5f5a4becc94e64c4cb0521137ebc182
andr68rus <ashtmb@gmail.com>
2018-11-05 08:44:48 +0300

    msm8996-common: LeParts: clean

--- parts/src/org/omnirom/device/Startup.java
+++ parts/src/org/omnirom/device/Startup.java
@@ -20,28 +20,8 @@ package org.omnirom.device;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.preference.PreferenceManager;
-import android.provider.Settings;
-import android.text.TextUtils;
 
 public class Startup extends BroadcastReceiver {
-
-    private void restore(String file, boolean enabled) {
-        if (file == null) {
-            return;
-        }
-        if (enabled) {
-            Utils.writeValue(file, "1");
-        }
-    }
-
-    private void restore(String file, String value) {
-        if (file == null) {
-            return;
-        }
-        Utils.writeValue(file, value);
-    }
 
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
