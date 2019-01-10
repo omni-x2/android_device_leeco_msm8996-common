022f0b8c50254d14fe70c6e43abc69054ab91815
andr68rus <ashtmb@gmail.com>
2019-01-10 19:55:19 +0300

    msm8996-common: this included in nitrogen vendor

--- msm8996.mk
+++ msm8996.mk
@@ -338,13 +338,6 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
-PRODUCT_PACKAGES += \
-    ims-ext-common \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
